23abd53d44290d81a58f6fa76576c581951d7d2e
Optik163 <huolchanhung1234@gmail.com>
2024-11-02 13:03:19 +0700

    Add files via upload

---
+++ SQLAdventureWorks sample.sql
@@ -0,0 +1,96 @@
+﻿---Tính toán phần trăm thay đổi của mức lương (Rate) giữa lần tăng lương gần nhất và lần trước đó cho từng nhân viên										
+
+WITH CTE AS 
+( SELECT
+        BusinessEntityID,
+        PayRateNumber = ROW_NUMBER() OVER ( PARTITION BY BusinessEntityID ORDER BY RateChangeDate DESC ),
+        RateChangeDate,
+        Rate
+    FROM HumanResources.EmployeePayHistory)
+SELECT
+    R1.BusinessEntityID,
+    PriorRate = R2.Rate,
+    LatestRate = R1.Rate,
+    PercentChange = CONVERT(VARCHAR(10), (R1.Rate - R2.Rate) / R2.Rate * 100) + '%'
+FROM CTE R1
+LEFT JOIN CTE R2
+    ON R1.BusinessEntityID = R2.BusinessEntityID
+    AND R2.PayRateNumber = 2
+where R1.PayRateNumber = 1
+AND R2.Rate IS NOT NULL;
+
+---Tìm 5 nhân viên bán hàng có doanh thu cao nhất và đồng thời ghép cặp với 5 nhân viên bán hàng có doanh thu thấp nhất trong năm 2013								
+
+WITH SalesRank AS (
+    SELECT
+        SalesPersonID,
+        SalesTotal = SUM(SubTotal),
+        SalesRankTopDown = ROW_NUMBER() OVER ( ORDER BY SUM(Subtotal) DESC),
+        SalesRankBotUp = ROW_NUMBER() OVER ( ORDER BY SUM(Subtotal) )
+    FROM Sales.SalesOrderHeader
+    WHERE YEAR(OrderDate) = 2013
+    AND SalesPersonID IS NOT NULL
+    GROUP BY SalesPersonID
+)
+SELECT TOP 5
+ TopSalesPersonID = N1.SalesPersonID,
+    TopRevenue = N1.SalesTotal,
+    BotSalesPersonID = N2.SalesPersonID,
+    BotRevenue = N2.SalesTotal
+FROM SalesRank N1
+INNER JOIN SalesRank N2
+    ON N1.SalesRankTopDown = N2.SalesRankBotUp
+ORDER BY N1.SalesRankTopDown;
+
+---Lấy ra đơn hàng đầu tiên trong mỗi năm 2012, 2013 khi tổng doanh thu cộng dồn từ các đơn hàng vượt quá 10 triệu							
+	
+WITH CombinedData AS (
+    SELECT
+        YEAR(OrderDate) AS FiscalYear,
+        OrderDate = CAST(OrderDate AS DATE),
+        SalesOrderID,
+        OrderNumber = ROW_NUMBER() OVER (ORDER BY SalesOrderID),
+        RunningTotal = SUM(SubTotal) OVER (
+            PARTITION BY YEAR(OrderDate)
+            ORDER BY OrderDate, SalesOrderID
+            ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+        )
+    FROM Sales.SalesOrderHeader
+    WHERE YEAR(OrderDate) IN (2012, 2013)
+),
+RankedOrders AS (
+    SELECT
+        FiscalYear,
+        OrderDate,
+        SalesOrderID,
+        OrderNumber,
+        RunningTotal,
+        ROW_NUMBER() OVER (PARTITION BY FiscalYear ORDER BY OrderDate) AS RowNum
+    FROM CombinedData
+    WHERE RunningTotal >= 10000000
+)
+
+SELECT FiscalYear, OrderDate, SalesOrderID, OrderNumber, RunningTotal
+FROM RankedOrders
+WHERE RowNum = 1; 
+
+--Lấy ra 10% các lệnh sản xuất có tỷ lệ phế phẩm (scrap rate) lớn hơn 3%, sắp xếp theo ngày đáo hạn (DueDate) giảm dần							
+
+WITH WorkOrderData AS (
+    SELECT
+        WorkOrderID,
+        DueDate ,
+        ProdName = N2.Name,
+        ScrapReason = N3.Name,
+        ScrappedQty,
+        OrderQty,
+        PercScrapped = ROUND(CAST(ScrappedQty AS FLOAT) / OrderQty * 100, 2)
+    FROM Production.WorkOrder N1
+    JOIN Production.Product N2 ON N1.ProductID = N2.ProductID
+    LEFT JOIN Production.ScrapReason N3 ON N1.ScrapReasonID = N3.ScrapReasonID
+    WHERE CAST(ScrappedQty AS FLOAT) / OrderQty  > 0.03
+)
+SELECT TOP (10) PERCENT *
+FROM WorkOrderData
+ORDER BY DueDate DESC;
+
---
+++ SQLdonhang.sql
@@ -0,0 +1,161 @@
+﻿-- Tính tổng lượng đơn và doanh thu theo mã tỉnh đi							
+SELECT							
+MaTinhDi,							
+COUNT(MaDon) AS TongLuongDon,							
+SUM(CuocPhi) AS TongDoanhThu							
+FROM							
+Bang1							
+GROUP BY							
+MaTinhDi							
+ORDER BY							
+MaTinhDi;							
+							
+-- Tính tổng số lượng khách hàng, lượng đơn và doanh thu theo vùng							
+SELECT							
+t.TenVung AS Vung,							
+COUNT(DISTINCT dh.MaKhachHang) AS TongSoLuongKhachHang,							
+COUNT(dh.MaDon) AS TongLuongDon,							
+SUM(dh.CuocPhi) AS TongDoanhThu							
+FROM							
+Bang1 dh							
+JOIN							
+Bang2 t ON dh.MaTinhDi = t.MaTinh							
+GROUP BY							
+t.TenVung							
+ORDER BY							
+t.TenVung;							
+							
+-- Tính tổng số lượng khách hàng và doanh thu theo mức xếp hạng và vùng							
+SELECT							
+x.XepHang AS MucXepHang,							
+t.TenVung AS Vung,							
+COUNT(DISTINCT dh.MaKhachHang) AS TongSoLuongKhachHang,							
+SUM(dh.CuocPhi) AS TongDoanhThu							
+FROM							
+Bang1 dh							
+JOIN							
+Bang2 t ON dh.MaTinhDi = t.MaTinh							
+JOIN							
+Bang3 x ON dh.MaKhachHang = x.MaKhachHang AND dh.Thang = x.Thang							
+GROUP BY							
+x.XepHang,							
+t.TenVung							
+ORDER BY							
+x.XepHang,							
+t.TenVung;							
+							
+-- Tính số lượng khách hàng và doanh thu theo phân loại khách hàng							
+SELECT							
+CASE							
+WHEN x.XepHang = 'A' THEN N'Khách hàng lớn'							
+WHEN x.XepHang IN ('B', 'C') THEN N'Khách hàng vừa và nhỏ'							
+ELSE N'Khách hàng lẻ'							
+END AS PhanLoaiKhachHang,							
+COUNT(DISTINCT dh.MaKhachHang) AS TongSoLuongKhachHang,							
+SUM(dh.CuocPhi) AS TongDoanhThu							
+FROM							
+Bang1 dh							
+JOIN							
+Bang3 x ON dh.MaKhachHang = x.MaKhachHang AND dh.Thang = x.Thang							
+GROUP BY							
+CASE							
+WHEN x.XepHang = 'A' THEN N'Khách hàng lớn'							
+WHEN x.XepHang IN ('B', 'C') THEN N'Khách hàng vừa và nhỏ'							
+ELSE N'Khách hàng lẻ'							
+END							
+ORDER BY							
+PhanLoaiKhachHang;							
+							
+-- Tính tổng lượng đơn và doanh thu của mỗi khách hàng công nợ thuộc nhóm khách hàng lớn							
+SELECT							
+dh.MaKhachHang,							
+COUNT(dh.MaDon) AS TongLuongDon,							
+SUM(dh.CuocPhi) AS TongDoanhThu							
+FROM							
+Bang1 dh							
+JOIN							
+Bang3 x ON dh.MaKhachHang = x.MaKhachHang AND dh.Thang = x.Thang							
+JOIN							
+Bang4 cndn ON dh.MaKhachHang = cndn.MaKhachHang AND dh.Thang = cndn.Thang							
+WHERE							
+x.XepHang = 'A'							
+GROUP BY							
+dh.MaKhachHang							
+ORDER BY							
+TongDoanhThu DESC;							
+							
+-- Tìm các đơn hàng có mã tỉnh đi và đến khác nhau thuộc các vùng khác nhau							
+SELECT							
+dh.MaDon,							
+dh.MaTinhDi,							
+di.TenVung AS VungDi,							
+dh.MaTinhDen,							
+den.TenVung AS VungDen,							
+dh.CuocPhi							
+FROM							
+Bang1 dh							
+JOIN							
+Bang2 di ON dh.MaTinhDi = di.MaTinh							
+JOIN							
+Bang2 den ON dh.MaTinhDen = den.MaTinh							
+WHERE							
+di.TenVung <> den.TenVung							
+ORDER BY							
+dh.MaDon;							
+							
+-- Tìm các khách hàng có đơn hàng từ các tỉnh đi khác nhau thuộc cùng một vùng và đến chung một tỉnh							
+WITH KhachHangVaTinh AS (							
+SELECT							
+dh.MaKhachHang,							
+di.TenVung AS VungDi,							
+dh.MaTinhDen AS MaTinhDen,							
+COUNT(DISTINCT dh.MaTinhDi) AS SoTinhDi							
+FROM							
+Bang1 dh							
+JOIN							
+Bang2 di ON dh.MaTinhDi = di.MaTinh							
+JOIN							
+Bang2 den ON dh.MaTinhDen = den.MaTinh							
+GROUP BY							
+dh.MaKhachHang,							
+di.TenVung,							
+dh.MaTinhDen							
+)							
+-- Lọc các khách hàng có đơn hàng từ các tỉnh đi khác nhau thuộc cùng một vùng và đến chung một tỉnh							
+SELECT							
+MaKhachHang,							
+VungDi,							
+MaTinhDen,							
+SoTinhDi							
+FROM							
+KhachHangVaTinh							
+WHERE							
+SoTinhDi > 1							
+ORDER BY							
+MaKhachHang, VungDi, MaTinhDen;							
+							
+--Tính tổng doanh thu tích lũy cho mỗi tỉnh đến, phân theo vùng							
+WITH DoanhThuTichLuy AS (							
+SELECT							
+dh.MaTinhDen,							
+den.TenVung AS VungDen,							
+SUM(dh.CuocPhi) AS DoanhThu,							
+SUM(SUM(dh.CuocPhi)) OVER (PARTITION BY den.TenVung ORDER BY dh.Thang ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS TongDoanhThuTichLuy							
+FROM							
+Bang1 dh							
+JOIN							
+Bang2 den ON dh.MaTinhDen = den.MaTinh							
+GROUP BY							
+dh.MaTinhDen,							
+den.TenVung,							
+dh.Thang							
+)							
+SELECT							
+MaTinhDen,							
+VungDen,							
+DoanhThu,							
+TongDoanhThuTichLuy							
+FROM							
+DoanhThuTichLuy							
+ORDER BY							
+VungDen, MaTinhDen;							
